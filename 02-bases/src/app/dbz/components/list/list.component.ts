9542a9a0c02c18c0cd21f152ec86016012b6e5bf
Cesar-CCS <cesarcastro_s@outlook.com>
2024-04-30 16:51:57 -0500

    section 5 - 66. FormsModule & ngModel

---
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, input } from '@angular/core';
+import { Character } from '../../interfaces/characters.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  @Input()
+
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
+
+}
